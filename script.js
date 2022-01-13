16e27213cf4a952ebbab17dc5a144e907244245f
Justin Chu <justinchu24.com>
2022-01-13 11:28:56 -0500

    finished js logic and styling

--- script.js
+++ script.js
@@ -1,36 +1,126 @@
-let computerRandom = Math.floor(Math.random() * 3);
-let computerPlay;
-if (computerRandom == 0) {
-    computerPlay = 'rock'
-} else if (computerRandom == 1) {
-    computerPlay = 'paper'
-} else {
-    computerPlay = 'scissors'
+// Create Random computer choice
+function getComputerSelection (){
+    let computerRandom = Math.floor(Math.random() * 3);
+    if (computerRandom == 0) {
+        return  'rock';
+    } else if (computerRandom == 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
 }
+};
 
-let playerSelection = window.prompt('Choose your weapon:').toLowerCase()
 
+// Create Selection & Selection Events
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+
+
+// Create scoreboard
 let userScore = 0;
-let computerScore = 0;
+let compScore = 0;
+const user = document.getElementById('user-score');
+const comp = document.getElementById('comp-score');
+user.textContent = 0;
+comp.textContent = 0;
 
 
-function playGame(playerSelection, computerPlay){
-    if (playerSelection == computerPlay) {
-        return 'Tie.. should\'ve picked rock.'
-    } else if (playerSelection == 'rock' && computerPlay == 'paper') { 
-        return 'You lose to the weak paper'
-    } else if (playerSelection == 'rock' && computerPlay == 'scissors') {
-        return 'You win! Rock is superior to every object'
-    } else if (playerSelection == 'paper' && computerPlay == 'scissors') {
-        return 'You lose. Definitely should\'ve went with rock'
-    } else if (playerSelection == 'paper' && computerPlay == 'rock') {
-        return 'You win, but rock would have been more dramatic.'
-    } else if (playerSelection == 'scissors' && computerPlay == 'rock'){
-        return 'You lose. Obviouly rock is the strongest'
-    } else if (playerSelection == 'scissors' && computerPlay == 'paper') {
-        return 'You win, you\'re just lucky you didn\'t face a rock.'
+
+// Create images under the winner
+const userCheck = document.getElementById('user-check');
+const compCheck = document.getElementById('comp-check');
+userCheck.style.visibility = 'hidden';
+compCheck.style.visibility = 'hidden';
+
+const output = document.getElementById('output-statement');
+
+// Update winner statement
+function resetScore(){
+    setTimeout(()=> {
+        userScore = 0;
+        compScore = 0;
+        user.textContent = 0;
+        comp.textContent = 0;}, 2500)
+};
+
+const winner = document.getElementById('winner-statement');
+function decideWinner(user, comp){
+    if (compScore == 5){
+        winner.textContent = 'The computer wins :(';
+        resetScore();
+    } else if (userScore == 5){
+        winner.textContent = 'You beat that stupid computer! :)';
+        resetScore();
+    } else {
+        winner.textContent = 'It\'s anyone\'s game!'
     }
 };
 
-console.log(`computer choice: ${computerPlay}`)
-console.log(playGame(playerSelection, computerPlay))
+
+
+
+function win() {
+    userScore++;
+    user.textContent = userScore;
+    userCheck.src = '/images/greencheck.png';
+    userCheck.style.visibility = 'visible';
+    compCheck.style.visibility = 'hidden';
+    output.textContent = 'You Win!'
+};
+function lose() {
+    compScore++;
+    comp.textContent = compScore;
+    compCheck.src = '/images/greencheck.png';
+    compCheck.style.visibility = 'visible';
+    userCheck.style.visibility = 'hidden';
+    output.textContent = 'You lose..'
+};
+function tie() {
+    compCheck.style.visibility = 'hidden';
+    userCheck.style.visibility = 'hidden';
+    output.textContent = 'It\'s a draw.'
+
+};
+
+
+
+function game(userSelection) {
+    const computerSelection = getComputerSelection();
+    switch (userSelection + computerSelection) {
+        case 'rockscissors':
+        case 'paperrock':
+        case 'scissorspaper':
+            win();
+            break;
+        case 'rockpaper':
+        case 'scissorsrock':
+        case 'paperscissors':
+            lose();
+            break;
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+            tie();
+            break;
+    }
+    decideWinner(userScore, compScore);
+};
+
+function play(userSelection) {
+    rock.addEventListener('click', function(){
+        game('rock');
+    });
+    paper.addEventListener('click', function(){
+        game('paper');
+    });
+    scissors.addEventListener('click', function(){
+        game('scissors');
+    });
+};
+
+play();
+
+
+
+
